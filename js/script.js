f0e4580f12d2ae86612b9a7da5eb1c4f62db72bc
Justin He <hejustin@usc.edu>
2018-08-23 15:39:36 -0400

    website update

--- js/script.js
+++ js/script.js
@@ -2,23 +2,60 @@ $(document).ready(function(){
   $("body").fadeIn(3000);
   $('.collapse').collapse()
   $("#aboutId").css("opacity","0");
-  $("#scheduleId").css("opacity","0");
-  $("#footerId").css("opacity","0");
+  $("#programId").css("opacity","0");
+  $("#faqId").css("opacity","0");
+  $("#teamId").css("opacity","0");
+  $("#sponsorsId").css("opacity","0");
     $(window).scroll(function(){
       var top_of_object = $("#aboutId").position().top + $("#aboutId").outerHeight();
       var bottom_of_window = $(window).scrollTop() + $(window).height();
       if(bottom_of_window > top_of_object){
         $("#aboutId").animate({opacity:1}, 1000);
       }
-      var top_of_object = $("#scheduleId").position().top + $("#scheduleId").outerHeight();
+      var top_of_object = $("#programId").position().top + $("#programId").outerHeight();
       var bottom_of_window = $(window).scrollTop() + $(window).height();
       if(bottom_of_window > top_of_object){
-        $("#scheduleId").animate({opacity:1}, 1000);
+        $("#programId").animate({opacity:1}, 1000);
       }
-      var top_of_object = $("#footerId").position().top + + $("#footerId").outerHeight();
+      var top_of_object = $("#faqId").position().top + $("#faqId").outerHeight()/4;
       var bottom_of_window = $(window).scrollTop() + $(window).height();
       if(bottom_of_window > top_of_object){
-        $("#footerId").animate({opacity:1}, 1000);
+        $("#faqId").animate({opacity:1}, 1000);
+      }
+      var top_of_object = $("#teamId").position().top + $("#teamId").outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      if(bottom_of_window > top_of_object){
+        $("#teamId").animate({opacity:1}, 1000);
+      }
+      var top_of_object = $("#sponsorsId").position().top + $("#sponsorsId").outerHeight()/8;
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      if(bottom_of_window > top_of_object){
+        $("#sponsorsId").animate({opacity:1}, 1000);
       }
     });
+  $( "#AboutNav" ).click(function() {
+    $('html, body').animate({
+      scrollTop: $("#aboutId").offset().top - 20
+    }, 1000);
+  });
+  $( "#ProgramNav" ).click(function() {
+    $('html, body').animate({
+      scrollTop: $("#programId").offset().top - 20
+    }, 1000);
+  });
+  $( "#FAQNav" ).click(function() {
+    $('html, body').animate({
+      scrollTop: $("#faqId").offset().top - 20
+    }, 1000);
+  });
+  $( "#TeamNav" ).click(function() {
+    $('html, body').animate({
+      scrollTop: $("#teamId").offset().top - 20
+    }, 1000);
+  });
+  $( "#SponsorsNav" ).click(function() {
+    $('html, body').animate({
+      scrollTop: $("#sponsorsId").offset().top - 20
+    }, 1000);
+  });
 });
